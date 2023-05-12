afdfbf6d52b57ee2ac869e1fe0bf9ec37ea198df
n-tirrell <ntirrel1@jh.edu>
2023-05-11 21:02:14 -0400

    Multi-output option

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 
 // this function executes our search via an AJAX call
-function runSearch() {
+function runSearch(dtype) {
     // hide and clear the previous results, if any
     $('#results').hide();
     $('tbody').empty();
@@ -12,8 +12,8 @@ function runSearch() {
         url: './search_product.cgi',
         dataType: 'json',
         data: frmStr,
-        success: function (data, textStatus, jqXHR) {
-            processJSON(data);
+        success: function (data, dtype, textStatus, jqXHR) {
+            processJSON(data, dtype);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             alert("Failed to perform gene search! textStatus: (" + textStatus +
@@ -22,77 +22,107 @@ function runSearch() {
     });
 }
 
+// this function creates the table for the variant data
+function make_variant_table_head(table, data) {
+    let thead = table.createTHead();
+    $('<th/>', { "text": "Patient Name" }).appendTo('thead');
+    $('<th/>', { "text": "Variant ID" }).appendTo('thead');
+    $('<th/>', { "text": "Gene ID" }).appendTo('thead');
+    $('<th/>', { "text": "Gene Name" }).appendTo('thead');
+    $('<th/>', { "text": "Gene Product" }).appendTo('thead');
 
-// this processes a passed JSON structure representing gene matches and draws it
-//  to the result table
-function processJSON(data) {
+}
+
+// this function creates the table for the model data
+function make_model_table_head(table) {
+
+    // create a row and append it to the body of the table
+    $('<th/>', { "text": "Patient Name" }).appendTo('thead');
+    $('<th/>', { "text": "Variant ID" }).appendTo('thead');
+    $('<th/>', { "text": "Process ID" }).appendTo('thead');
+    $('<th/>', { "text": "Process Name" }).appendTo('thead');
+    $('<th/>', { "text": "Model ID" }).appendTo('thead');
+    $('<th/>', { "text": "Model Name" }).appendTo('thead');
+
+}
+
+// this processes a passed JSON structure representing gene matches and draws it to the result table
+function processJSON(data, dtype) {
+    //populate the rows
     $('tbody').empty();
     
     // set the span that lists the match count
     $('#match_count').text(data.match_count);
 
-    // this will be used to keep track of row identifiers
-    var next_row_num = 1;
+    if (data.match_count > 0)
+    {
+        $("#results_header").empty();
+        let headers = Object.keys(data["matches"][0])
+        headers.forEach(header => {
+            $('<th/>', { 'text': header }).appendTo($('#results_header'));
+        });
 
-    // iterate over each match and add a row to the result table for each
-    $.each(data.matches, function (i, item) {
-        var this_row_id = 'result_row_' + next_row_num++;
+        data["matches"].forEach(row => {
+            let table_row = $('<tr/>')
 
-        // create a row and append it to the body of the table
-        $('<tr/>', { "id": this_row_id }).appendTo('tbody');
+            Object.keys(row).forEach(key => {
+                $('<td/>', { "text": row[key] }).appendTo(table_row);
+            });
 
-        // add the actual columns with data we want
-        $('<td/>', { "text": item.patient_name }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.variant_id }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.gene_id }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.gene_name }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.gene_product }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.process_id }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.process_name }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.model_id }).appendTo('#' + this_row_id);
-        $('<td/>', { "text": item.model_name }).appendTo('#' + this_row_id);
+            table_row.appendTo('tbody');
+        });
+        $('#results_table').show();
+    } else {     
+        $('#results_table').hide();
+    }
 
+    $('#results').show();
 
-    });
+    // create the appropriate table
+    // if (dtype = 'Variant Data'){
+    //     make_variant_table()
+    // } else {
+    //     make_model_table()
+    // }
+
+    // // this will be used to keep track of row identifiers
+    // var next_row_num = 1;
+
+    // // iterate over each match and add a row to the result table for each
+    // $.each(data.matches, function (i, item) {
+    //     var this_row_id = 'result_row_' + next_row_num++;
+
+    //     // create a row and append it to the body of the table
+    //     $('<tr/>', { "id": this_row_id }).appendTo('tbody');
+
+    //     // add the actual columns with data we want
+    //     $('<td/>', { "text": item.patient_name }).appendTo('#' + this_row_id);
+    //     $('<td/>', { "text": item.variant_id }).appendTo('#' + this_row_id);
+
+    //     // filter which information we show
+    //     if (dtype = "Variant Data"){
+    //         $('<td/>', { "text": item.gene_id }).appendTo('#' + this_row_id);
+    //         $('<td/>', { "text": item.gene_name }).appendTo('#' + this_row_id);
+    //         $('<td/>', { "text": item.gene_product }).appendTo('#' + this_row_id);
+    //     } else if (dtype = "Model Data"){
+    //         $('<td/>', { "text": item.process_id }).appendTo('#' + this_row_id);
+    //         $('<td/>', { "text": item.process_name }).appendTo('#' + this_row_id);
+    //         $('<td/>', { "text": item.model_id }).appendTo('#' + this_row_id);
+    //         $('<td/>', { "text": item.model_name }).appendTo('#' + this_row_id);
+    //     }
+
+    // });
 
     // now show the result section that was previously hidden
-    $('#results').show();
 }
 
 
 
 // run our javascript once the page is ready
 $(document).ready(function () {
-    console.log('Hello')
     // define what should happen when a user clicks submit on our search form
     $('#submit').click(function () {
         runSearch();
         return false;  // prevents 'normal' form submission
-    });
-
-    $('#search_box').autocomplete({
-        source: function (request, response) {
-            var frmStr = $('#variant_search').serializeArray();
-
-            frmStr.push({ name: 'ac', value: true });
-
-            $.ajax({
-                url: "search_product.cgi",
-                dataType: "json",
-                data: frmStr,
-                success: function (data) {
-                    options = []
-                    Object.keys(data.matches).forEach((key) => {
-                        options.push(data.matches[key].product);
-                    });
-                    response(options);
-                    console.log(options);
-                }
-            });
-        },
-        minLength: 2,
-        select: function (event, ui) {
-            $('#search_box').val(ui.item.value);
-        }
     });
 });
--- search_product.cgi
+++ search_product.cgi
@@ -3,18 +3,33 @@
 import cgi, json
 import mysql.connector
 
-def main():
-
-    print("Content-Type: application/json\n\n")
-    form = cgi.FieldStorage()
-    variant = form.getvalue('search_term')
-    patient = form.getvalue('patient_term')
-        
-    conn = mysql.connector.connect(user='ntirrel1', password='Ntjhu98&', host='localhost', database='ntirrel1_final')
-    cursor = conn.cursor()
-    
+# this function will vary based on the data we want
+def get_variant_info(cursor, variant, patient):
     qry = """
-            SELECT p.patient_name, v.variant_id,  g.gene_id, g.gene_name, g.gene_product, r.process_id, r.process_name, m.model_id, m.model_name
+            SELECT p.patient_name , v.variant_id,  g.gene_id, g.gene_name, g.gene_product
+                FROM variant v
+                    JOIN patient_variant pv on pv.variant_id = v.variant_id
+                    JOIN patient p on pv.patient_id = p.patient_id
+                    JOIN gene g ON v.gene_id = g.gene_id
+                    JOIN process_gene gp ON g.gene_id = gp.gene_id
+                    JOIN process r ON gp.process_id = r.process_id
+                    JOIN model m ON r.model_id = m. model_id
+                WHERE v.variant_id = %s
+                    AND p.patient_DOB = %s
+    """
+    cursor.execute(qry, (variant, patient))
+
+    results = { 'match_count': 0, 'matches': list(), 'dtype': 'variant' }
+
+    for (patient_name, variant_id, gene_id, gene_name, gene_product) in cursor:
+        results['matches'].append({'Patient Name': patient_name, 'variant_id': variant_id, 'gene_id': gene_id, 'gene_name': gene_name, 'gene_product': gene_product})
+        results['match_count'] += 1
+
+    return results
+
+def get_model_info(cursor, variant, patient):
+    qry = """
+            SELECT p.patient_name, v.variant_id, r.process_id, r.process_name, m.model_id, m.model_name
                 FROM variant v
                     JOIN patient_variant pv on pv.variant_id = v.variant_id
                     JOIN patient p on pv.patient_id = p.patient_id
@@ -28,11 +43,30 @@ def main():
 
     cursor.execute(qry, (variant, patient))
 
-    results = { 'match_count': 0, 'matches': list() }
+    results = { 'match_count': 0, 'matches': list(), 'dtype': 'model' }
 
-    for (patient_name, variant_id, gene_id, gene_name, gene_product, process_id, process_name, model_id, model_name ) in cursor:
-        results['matches'].append({'patient_name': patient_name, 'variant_id': variant_id, 'gene_id': gene_id, 'gene_name': gene_name, 'gene_product': gene_product, 'process_id':process_id, 'process_name': process_name, 'model_id': model_id, 'model_name': model_name})
+    for (patient_name, variant_id, process_id, process_name, model_id, model_name ) in cursor:
+        results['matches'].append({'patient_name': patient_name, 'variant_id': variant_id, 'process_id':process_id, 'process_name': process_name, 'model_id': model_id, 'model_name': model_name})
         results['match_count'] += 1
+
+    return results
+
+def main():
+
+    print("Content-Type: application/json\n\n")
+    form = cgi.FieldStorage()
+    variant = form.getvalue('search_term')
+    patient = form.getvalue('patient_term')
+    data = form.getvalue('data')
+        
+    conn = mysql.connector.connect(user='ntirrel1', password='Ntjhu98&', host='localhost', database='ntirrel1_final')
+    cursor = conn.cursor()
+
+    # get the information from the MySQL database
+    if data == 'variant': # get variant info
+        results = get_variant_info(cursor, variant, patient)
+    else: # get model info
+        results = get_model_info(cursor, variant, patient)
 
     conn.close()
 
